3df06166ee9a8c98f640b85339ee74e2b4ab96a1
Yelmuf <yelmuf@gmail.com>
2017-03-26 23:30:28 +0200

    Write in C

--- main.c
+++ main.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-#include <iostream> // only for cout
-
-using namespace std;
 
 int pow(int a, int b) {
     /// Power for integers
@@ -17,10 +14,32 @@ int pow(int a, int b) {
     };
 }
 
-bool handleInput(long input, int m, int p) {
+long min(long a, long b) {
+    return a < b ? a : b;
+}
+
+int inputCP(int def) {
+  /// Handle input of number from 1 to 9
+  // TODO: deal with scanf() overflow
+  int n = 0, s = 0;
+  while(1) {
+    s = scanf("%i", &n);
+    if(n > 0 && n < 10) {
+      return n;
+    } else if (s) {
+      printf("[!] Please enter number from 1 to 9\n");
+    } else {
+      printf("[!] Your input may have caused overflow. Using default number (%i)\n", def);
+      break;
+    }
+  }
+  return def;
+}
+
+
+_Bool handleInput(long input, int m, int p) {
     for (int i = 0; i < p; ++i) {
         long num = input / pow(10, p - i - 1);
-        // cout << "( num " << num << " < " << m << " ? )" << endl;
         if (num >= m || num < 0)
             return 0;
         input -= num * pow(10, p - i - 1);
@@ -31,8 +50,8 @@ bool handleInput(long input, int m, int p) {
 int testCode(long guess, long code, int p) {
     /// Test code for # of black and white pegs
     /// Returns # of black and white pegs
-    int g[p] = {};
-    int c[p] = {};
+    int g[p];
+    int c[p];
     // Decompose a number to p numbers by their places
     for (int i = 0; i < p; ++i) {
         g[i] = guess / pow(10, p - i - 1);
@@ -56,22 +75,6 @@ int testCode(long guess, long code, int p) {
         occurring[i] = min(occurring[i], occurringC[i]);
     }
 
-//    // debug
-//    cout << "testCode(";
-//    for (int j = 0; j < p; ++j) {
-//        cout << g[j];
-//    }
-//    cout << ",";
-//    for (int j = 0; j < p; ++j) {
-//        cout << c[j];
-//    }
-//    cout << ")__" << endl;
-//    cout << "occurring: ";
-//    for (int i = 0; i < 10; ++i) {
-//        cout << occurring[i] << " ";
-//    }
-//    // debug end
-
     int blacks = 0;
     for (int i = 0; i < p; ++i) {
         if (g[i] == c[i]) {
@@ -85,9 +88,6 @@ int testCode(long guess, long code, int p) {
             whites += occurring[i];
     }
 
-//  // debug return
-//    cout << endl << "__return " << blacks*10 + whites
-//         << " (" << blacks << " blacks and " << whites << " whites)" << endl;
     return blacks * 10 + whites;
 }
 
@@ -104,7 +104,7 @@ long composeNum(long j, int m) {
     return r;
 }
 
-int countActive(bool *active, int length) {
+int countActive(_Bool *active, int length) {
     int count = 0;
     for (int i = 0; i < length; ++i) {
         if (active[i])
@@ -120,13 +120,12 @@ void initArrayFromZero(long *a, int n, int m) {
     }
 }
 
-int testNumber(long testingNumber, long *b, int length, bool *active, int p) {
+int testNumber(long testingNumber, long *b, int length, _Bool *active, int p) {
     /// Find maximum score of number to test: https://programmingpraxis.com/2009/11/20/master-mind-part-2/
-//    cout << "Testing number " << testingNumber << ":" << endl;
     int maximum = 0;
     for (int i = 0; i < 40; ++i) {
         int counter = 0;
-        bool f = (composeNum(i, 5) / 10 + composeNum(i, 5)) < 5;
+        _Bool f = (composeNum(i, 5) / 10 + composeNum(i, 5)) < 5;
         for (int j = 0; j < length; ++j) {
             if (!active[i] && f) {
                 if (testCode(testingNumber, b[j], p) == composeNum(i, 5)) {
@@ -136,24 +135,24 @@ int testNumber(long testingNumber, long *b, int length, bool *active, int p) {
         }
         if (counter > maximum)
             maximum = counter;
-//        cout << counter << endl;
     }
-//    cout << "max: " << maximum << endl;
     return maximum;
 }
 
 
-void setNumbersWithSameCodeActive(long number, long code, long *b, int length, bool *active, int p) {
+void setNumbersWithSameCodeActive(long number, long code, long *b, int length, _Bool *active, int p) {
     /// For each number with the same # of blacks and whites set "active"
     for (int i = 0; i < length; ++i) {
         if (testCode(b[i], number, p) == code) {
             active[i] = 1;
+        } else {
+          active[i] = 0;
         }
     }
 }
 
 
-void excludeNumbersWithSameCode(long number, long code, long *b, int length, bool *active, int p) {
+void excludeNumbersWithSameCode(long number, long code, long *b, int length, _Bool *active, int p) {
     /// For each number with not the same # of blacks and whites set "not active"
     for (int i = 0; i < length; ++i) {
         if (active[i] && (testCode(b[i], number, p) != code)) {
@@ -162,22 +161,21 @@ void excludeNumbersWithSameCode(long number, long code, long *b, int length, boo
     }
 }
 
-int minimax(long *b, int length, bool *active, int p) {
+int minimax(long *b, int length, _Bool *active, int p) {
     /// Minimax explanation: https://programmingpraxis.com/2009/11/20/master-mind-part-2/
     long minimum = -1;
     int minimumNumber = -1;
     // set initial minimum11
     int i = 0;
-    for (i; i < length; ++i) {
+    for (int i = i; i < length; ++i) {
         if (active[i]) {
             minimum = testNumber(b[i], b, length, active, p);
             minimumNumber = b[i];
-//            cout << "broke from min search loop with b[i] = " << b[i] << "[" << i << "]" << endl; // debug
             break;
         }
     }
 
-    for (i; i < length; ++i) {
+    for (int i = i; i < length; ++i) {
         if (!active[i])
             continue;
         else {
@@ -185,7 +183,7 @@ int minimax(long *b, int length, bool *active, int p) {
             if (minimum > maximum) {
                 minimum = maximum;
                 minimumNumber = b[i];
-                cout << "    new minimax<-" << minimumNumber << " ";
+                printf("    new minimax<-%i ", minimumNumber);
             }
         }
     }
@@ -194,36 +192,35 @@ int minimax(long *b, int length, bool *active, int p) {
 
 void printResult(long result) {
     /// This does what you think it does
-    cout << endl << "Your number is ";
-    printf("%li", result);
-    cout << endl << "GGWP" << endl;
+    printf("\nYour number is %li.\nGGWP\n", result);
 }
 
 
 int main() {
-    cout << "x---------------------------------------x" << endl;
-    cout << "|     Welcome to mastermind-solver!     |" << endl;
-    cout << "x---------------------------------------x" << endl;
+    printf("x---------------------------------------x\n");
+    printf("|     Welcome to mastermind-solver!     |\n");
+    printf("x---------------------------------------x\n\n");
 
-    /// Change your rules here ////////////////////////////////////////////
-    int m = 6; // {0,1,2,3,4,5} = 6 colors, // 10 is maximum             //
-    int p = 4; // 0052 = 4 places (digits), // higher is slower          //
-    ///////////////////////////////////////////////////////////////////////
+    printf("Enter number of colors:\n");
+    int m = inputCP(6);  // {0,1,2,3,4,5} = 6 colors, 10 is maximum
+    printf("Enter number of places:\n");
+    int p = inputCP(4);  // 0052 = 4 places (digits), higher is slower
 
+    printf("\nNow playing with %i colors and %i places\n", m, p);
 
-    cout << "Now playing with " << m << " colors and " << p << " places" << endl;
     int length = pow(m, p); // 6^4 possibilities
 
     // Initialization of possibilities
-    bool active[length] = {0}; // array of booleans to exclude in next search
+    _Bool active[length]; // array of _Booleans to exclude in next search
     long b[length];
     initArrayFromZero(b, length, m);
-    cout << "Initialized " << length << " possibilities" << endl;
-    cout << "Array tail: ";
+
+    printf("Initialized %i possibilities\n", length);
+    printf("Array tail: ");
     for (int i = 5; i > 0; i--) {
-        cout << b[length - i] << " ";
+        printf("%li ", b[length - i]);
     }
-    cout << endl;
+    printf("\n");
 
 //    cout << "// Some active testings: " << active[0] << " " << active[length/2] << " "
 //         << active[length-1] << " (should be all zeros)" << endl;
@@ -231,10 +228,10 @@ int main() {
 
     // Handle input
     long input;
-    cout << endl << "Enter your combination:" << endl;
+    printf("\nEnter your combination:\n");
     scanf("%li", &input);
     while (!handleInput(input, m, p)) {
-        cout << "Wrong code format, check number of colors and places, then enter code again:" << endl;
+        printf("[!] Wrong code format, check number of colors and places, then enter code again:\n");
         scanf("%li", &input);
     }
 
@@ -244,12 +241,12 @@ int main() {
     long teste = 11;
     // long teste = minimax(b, length, active, p); // 9 seconds on core-i5 for p=4, m=6, much more for bigger numbers
 
-    cout << "Here's a starting minimax: " << teste << endl;
+    printf("Here's a starting minimax: %li\n", teste);
 
 
     for (int krok = 1; krok <= 10; krok++) {
 
-        cout << endl << "[" << krok << "] Testing code " << teste << endl;
+        printf("\n[%i] Testing code %li\n", krok, teste);
 
         int r = testCode(teste, input,
                          p); // Result is numbers of white and black pegs (hits) for current test code (teste now)
@@ -261,12 +258,12 @@ int main() {
             break;
         }
 
-        cout << "    It's not an answer! Searching numbers to choose from ";
+        printf("    It's not an answer! Searching numbers to choose from ");
         if (krok == 1)
-            cout << "all";
+            printf("all");
         else
-            cout << countActive(active, length);
-        cout << " active.." << endl << "    Excluding (" << teste << ", " << r << "... )" << endl;
+            printf("%i", countActive(active, length));
+        printf(" active..\n    Excluding (%li, %i ...)\n", teste, r);
 
         if (krok == 1)
             setNumbersWithSameCodeActive(teste, r, b, length, active, p);
@@ -277,12 +274,10 @@ int main() {
         // Apply the minimax rule to select next code to test
         long newTeste = minimax(b, length, active, p);
         if (newTeste == teste) {
-            cout << endl << "[E] Opps, got into an infinite loop, please report to author\nExiting immediately";
+            printf("\n[E] Opps, got into an infinite loop, please report to author\nExiting immediately\n");
             return 3;
         }
         teste = newTeste;
-
     }
-
     return 0;
 }
